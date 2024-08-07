0ff586c55cd3f3d7206047c40c656c435692484f
ihpar <ismail.hakki.parlak@gmail.com>
2024-08-07 11:26:35 +0300

    deletes old files

--- constants.py
+++ constants.py
@@ -2,3 +2,7 @@ SR = 48000
 N_FFT = 512
 HOP = 256
 NUM_SAMPLES = 4800
+NEY_DIR = "dataset/ney/"
+GTR_DIR = "dataset/ac_gtr/"
+NEY_SPECTROGRAM_DIR = "dataset/spectrograms/ney/"
+GTR_SPECTROGRAM_DIR = "dataset/spectrograms/ac_gtr/"
