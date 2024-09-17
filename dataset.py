96d747011c19f6352ec8e5f7a3e0c009da4b1e5a
ihpar <ismail.hakki.parlak@gmail.com>
2024-09-17 14:46:30 +0300

    adds new experiments

--- dataset.py
+++ dataset.py
@@ -14,29 +14,36 @@ gtr_feature_dirs = sorted(
 x_train_dirs, x_test_dirs, y_train_dirs, y_test_dirs = train_test_split(
     gtr_feature_dirs, ney_feature_dirs, test_size=0.2, random_state=42)
 
-# print(len(x_train_dirs), len(x_test_dirs))
-# print(x_train_dirs[0], y_train_dirs[0])
-# print(x_test_dirs[0], y_test_dirs[0])
-
 
 class FeatureDataset(Dataset):
-    def __init__(self, x_dirs, y_dirs):
+    def __init__(self, x_dirs, y_dirs, part):
         self.x_dirs = x_dirs
         self.y_dirs = y_dirs
+        self.part = part
         self.x = None
         self.y = None
         self.file_paths_x = None
         self.file_paths_y = None
+        self.min_max_vals = {
+            "gtr": {"min": None, "max": None},
+            "ney": {"min": None, "max": None}
+        }
 
         # gtr files
-        features_arr, file_paths_x = self._build_feature_data(self.x_dirs)
+        features_arr, file_paths_x, max_gtr, min_gtr = self._build_feature_data(
+            self.x_dirs)
         self.x = features_arr
         self.file_paths_x = file_paths_x
+        self.min_max_vals["gtr"]["max"] = max_gtr
+        self.min_max_vals["gtr"]["min"] = min_gtr
 
         # ney files
-        features_arr, file_paths_y = self._build_feature_data(self.y_dirs)
+        features_arr, file_paths_y, max_ney, min_ney = self._build_feature_data(
+            self.y_dirs)
         self.y = features_arr
         self.file_paths_y = file_paths_y
+        self.min_max_vals["ney"]["max"] = max_ney
+        self.min_max_vals["ney"]["min"] = min_ney
 
     def _build_feature_data(self, dir_paths):
         features_arr = []
@@ -47,9 +54,12 @@ class FeatureDataset(Dataset):
             paths_arr.extend(file_paths)
 
         features_arr = np.array(features_arr, dtype=np.float32)
+        min_val = np.min(features_arr)
+        max_val = np.max(features_arr)
+        features_arr = (features_arr - min_val) / (max_val - min_val)
         features_arr = np.expand_dims(features_arr, axis=1)
         paths_arr = np.array(paths_arr)
-        return features_arr, paths_arr
+        return features_arr, paths_arr, max_val, min_val
 
     def _get_features_in_dir(self, dir_path):
         features = []
@@ -59,11 +69,16 @@ class FeatureDataset(Dataset):
         for f in sorted_files:
             with open(f, "rb") as handle:
                 chunk = pickle.load(handle)
-                signal = (chunk["signal"] + 1.0) / 2.0
-                if len(signal) < WINDOW_SAMPLE_LEN:
-                    signal = librosa.util.fix_length(
-                        signal, size=WINDOW_SAMPLE_LEN)
-                features.append(signal)
+                if self.part == "abs":
+                    # extract abs and convert to db
+                    stft_data = librosa.power_to_db(chunk["abs"])
+                elif self.part == "angle":
+                    # extract angle
+                    stft_data = chunk["angle"]
+                else:
+                    raise Exception(
+                        "part parameter must be either `abs` or `angle`")
+                features.append(stft_data)
                 file_paths.append(str(f))
         return features, file_paths
 
@@ -74,15 +89,18 @@ class FeatureDataset(Dataset):
         return self.x.shape[0]
 
 
-train_dataset = FeatureDataset(x_train_dirs, y_train_dirs)
-test_dataset = FeatureDataset(x_test_dirs, y_test_dirs)
+def build_data_loaders(part):
+    train_dataset = FeatureDataset(x_train_dirs, y_train_dirs, part)
+    test_dataset = FeatureDataset(x_test_dirs, y_test_dirs, part)
 
-train_data_loader = DataLoader(
-    dataset=train_dataset,
-    batch_size=8,
-    shuffle=True)
+    train_data_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=4,
+        shuffle=True)
 
-test_data_loader = DataLoader(
-    dataset=test_dataset,
-    batch_size=8,
-    shuffle=False)
+    test_data_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=4,
+        shuffle=False)
+
+    return train_data_loader, test_data_loader
