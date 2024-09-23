2a4630c627710f2608beb67694d34f2a54829a5b
ihpar <ismail.hakki.parlak@gmail.com>
2024-09-23 12:09:51 +0300

    first set of experiments with the new models

--- utils.py
+++ utils.py
@@ -7,19 +7,20 @@ def stitch_wave_chunks(waves):
     Takes wave chunks as a list and stitches them with cross-fades.
     """
     stitched_wave = np.array([])
-    fade_in = np.linspace(0, 1, FADE_SAMPLE_LEN)
-    fade_out = np.linspace(1, 0, FADE_SAMPLE_LEN)
+    fade_in = np.linspace(0, 1, WINDOW_SLIDE_MARGIN)
+    fade_out = np.linspace(1, 0, WINDOW_SLIDE_MARGIN)
 
     for i, wave in enumerate(waves):
         if i > 0:
             # fade in wave chunks after the 1st one
-            wave[:FADE_SAMPLE_LEN] *= fade_in
+            wave[:WINDOW_SLIDE_MARGIN] *= fade_in
         # fade out all wave chunks
-        wave[-FADE_SAMPLE_LEN:] *= fade_out
+        wave[-WINDOW_SLIDE_MARGIN:] *= fade_out
 
         if len(stitched_wave) > 0:
-            stitched_wave[-FADE_SAMPLE_LEN:] += wave[:FADE_SAMPLE_LEN]
-            stitched_wave = np.append(stitched_wave, wave[FADE_SAMPLE_LEN:])
+            stitched_wave[-WINDOW_SLIDE_MARGIN:] += wave[:WINDOW_SLIDE_MARGIN]
+            stitched_wave = np.append(
+                stitched_wave, wave[WINDOW_SLIDE_MARGIN:])
         else:
             stitched_wave = np.append(stitched_wave, wave)
 
