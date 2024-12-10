1718003c4d461d0e9ce38612d78cc471a02761f3
ihpar <ismail.hakki.parlak@gmail.com>
2024-12-10 12:30:00 +0000

    train subpix fail

--- models/sub_pix.py
+++ models/sub_pix.py
@@ -44,10 +44,10 @@ class UNetWithSubpixel(nn.Module):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            nn.LeakyReLU(inplace=True)
         )
 
     def forward(self, x):
