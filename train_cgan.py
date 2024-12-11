b65d79535d8d02e08d736ef9ce868402c8a0811e
ihpar <ismail.hakki.parlak@gmail.com>
2024-12-11 17:23:05 +0000

    trains gan 0.5

--- train_cgan.py
+++ train_cgan.py
@@ -71,9 +71,8 @@ def train_cgan(device,
             running_gen += g_loss.item()
             running_disc += d_loss.item()
             num_batches += 1
-            if num_batches % 100 == 0:
-                print(f" - B: {num_batches}\t D: {d_loss.item():.6f}\t G: {
-                      g_loss.item():.6f}\t Gadv: {g_adv_loss.item():.6f}")
+            if num_batches % 200 == 0:
+                print(f" - B: {num_batches}\t D: {d_loss.item():.6f}\t G: {g_loss.item():.6f}\t Gadv: {g_adv_loss.item():.6f}")
 
         running_gen /= num_batches
         running_disc /= num_batches
